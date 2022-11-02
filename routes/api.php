77113771e2e9eaa44d5ab7b53555f298298624d9
Alexey <lehavorotuncev@gmail.com>
2022-11-02 18:18:15 +0300

    Add userresource json to GET /api/user

--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,7 @@
 <?php
 
+use App\Http\Response;
+use App\Resources\UserResource;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -18,7 +20,9 @@ Route::middleware('auth:sanctum')->group(function() {
 
     // текущий авторизованный пользователь
     Route::get('/user', function (Request $request) {
-        return $request->user();
+        $user = $request->user();
+        $user->load(['role', 'projects']);
+        return Response::success()->data(UserResource::makeArray($user));
     });
 
     /**
